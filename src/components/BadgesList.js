4a2c006d276a1ec4afec4d99e68ae5a7701adf36
Cesar Guerron <guerronc@outlook.com>
2019-07-19 20:54:14 -0500

    Agregar llamada a una API

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link } from "react-router-dom";
 import './styles/BadgesList.css';
 
 class BadgesListItem extends React.Component {
@@ -25,8 +26,44 @@ class BadgesListItem extends React.Component {
   }
 }
 
+class BadgesListItemApi extends React.Component {
+  render() {
+
+    console.log('BadgesListItemApi:', this.props.badge);
+    return (
+      <div className="BadgesListItem">
+        <img
+          className="BadgesListItem__avatar"
+          src={this.props.badge.image}
+          alt={`${this.props.badge.name}`}
+        />
+
+        <div>
+          <strong>
+            {this.props.badge.name} {this.props.badge.lastName}
+          </strong>
+          <br />@{this.props.badge.name}
+          <br />
+          {this.props.badge.species}
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
 class BadgesList extends React.Component {
   render() {
+    console.log('this.props.badges: ', this.props.badges)
+    if(this.props.badges.length === 0){
+      return(
+        <div>
+          <h3>No badges were found</h3>
+          <Link className='btn btn-primary' to='/badges/new'>New</Link>
+        </div>
+      );
+    }
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,81 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/Badges.css';
-import confLogo from '../images/badge-header.svg';
-import BadgesList from '../components/BadgesList';
+import "./styles/Badges.css";
+import confLogo from "../images/badge-header.svg";
+import BadgesList from "../components/BadgesList";
+
+import Api from "../api";
 
 class Badges extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('1. constructor()');
-
-    this.state = {
-      data: [],
-    };
-  }
+  state = {
+    loading: true,
+    error: null,
+    data: undefined,
+    nextPage: 1
+  };
 
   componentDidMount() {
-    console.log('3. componentDidMount()');
+    this.fetchData();
+    // this.fetchCharacter();
+  }
 
-    this.timeoutId = setTimeout(() => {
+  fetchCharacter = async () => {
+    this.setState({
+      loading: true,
+      error: null
+    });
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character?page=${this.state.nextPage}`
+      );
+      const data = await response.json();
+      console.log("Data procesada:", data.results);
       this.setState({
-        data: [
-          {
-            id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-            firstName: 'Freda',
-            lastName: 'Grady',
-            email: 'Leann_Berge@gmail.com',
-            jobTitle: 'Legacy Brand Director',
-            twitter: 'FredaGrady22221-7573',
-            avatarUrl:
-              'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-          },
-          {
-            id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-            firstName: 'Major',
-            lastName: 'Rodriguez',
-            email: 'Ilene66@hotmail.com',
-            jobTitle: 'Human Research Architect',
-            twitter: 'MajorRodriguez61545',
-            avatarUrl:
-              'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-          },
-          {
-            id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-            firstName: 'Daphney',
-            lastName: 'Torphy',
-            email: 'Ron61@hotmail.com',
-            jobTitle: 'National Markets Officer',
-            twitter: 'DaphneyTorphy96105',
-            avatarUrl:
-              'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-          },
-        ],
+        data: data.results,
+        loading: false,
+        nextPage: this.state.nextPage + 1
       });
-    }, 3000);
-  }
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: error
+      });
+    }
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log('5. componentDidUpdate()');
-    console.log({
-      prevProps: prevProps,
-      prevState: prevState,
+  fetchData = async () => {
+    this.setState({
+      loading: true,
+      error: null
     });
-
-    console.log({
-      props: this.props,
-      state: this.state,
-    });
-  }
-
-  componentWillUnmount() {
-    console.log('6. componentWillUnmount');
-    clearTimeout(this.timeoutId);
-  }
+    try {
+      const data = await Api.badges.list();
+      this.setState({
+        loading: false,
+        data: data
+      });
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        loading: false,
+        error: error
+      });
+    }
+  };
 
   render() {
-    console.log('2/4. render()');
+    if (this.state.loading) {
+      return "Loading...";
+    }
+
+    if (this.state.error) {
+      return `Error: ${this.state.error.message}`;
+    }
+
+    console.log("2/4. render()");
     return (
       <React.Fragment>
         <div className="Badges">
@@ -96,7 +95,6 @@ class Badges extends React.Component {
               New Badge
             </Link>
           </div>
-
           <BadgesList badges={this.state.data} />
         </div>
       </React.Fragment>
